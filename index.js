81695245794771904807dbec9797ba64e3da0aec
Charity Kiharo <charity.kiharo@student.moringaschool.com>
2026-04-14 13:05:47 +0300

    add merging date

--- index.js
+++ index.js
@@ -21,3 +21,20 @@ function combineUsers(...args) {
 
 // Export for testing
 module.exports = combineUsers;
+require('datejs');
+
+function combineUsers(...args) {
+  const combinedObject = {
+    users: []
+  };
+
+  args.forEach(arr => {
+    combinedObject.users.push(...arr);
+  });
+
+  combinedObject.merge_date = new Date().toString('M/d/yyyy');
+
+  return combinedObject;
+}
+
+module.exports = combineUsers;
