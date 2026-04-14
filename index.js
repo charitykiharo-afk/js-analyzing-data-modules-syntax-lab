fbcb906955655528a8cd09a2971015140c8e6ec8
charitykiharo-afk <charity.kiharo@student.moringaschool.com>
2026-04-14 15:26:15 +0300

    Refactor combineUsers and add mergeUsernames function

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
-function combineUsers(...args) {
-  const combinedObject = {
-    users: [],
-  };
-
-  args.forEach(arr => {
-    combinedObject.users.push(...arr);
-  });
-
-  combinedObject.merge_date = new Date().toDateString();
-
-  return combinedObject;
+function mergeUsernames(...args) {
+    const allUsers = [];
+    for (const usersArray of args) {
+        allUsers.push(...usersArray);
+    }
+    return allUsers;
 }
 
-module.exports = combineUsers;
+function combineUsers(...args) {
+    const combinedObject = {
+        users: []
+    };
+
+    for (const usersArray of args) {
+        combinedObject.users.push(...usersArray);
+    }
+
+    combinedObject.merge_date = new Date().toLocaleDateString();
+
+    return combinedObject;
+}
+
+const group1 = ["Lincon", "Ariel"];
+const group2 = ["Alvin", "Charity"];
+const group3 = ["Kerry"];
+
+console.log(mergeUsernames(group1, group2, group3));
+console.log(combineUsers(group1, group2, group3));
+
+module.exports = { mergeUsernames, combineUsers };
