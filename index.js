6d7fe9e8fb494fa51f3d9f7c1b539889a670e415
charitykiharo-afk <charity.kiharo@student.moringaschool.com>
2026-04-14 15:09:30 +0300

    Refactor combineUsers function and update date format
    Removed unnecessary imports and simplified date formatting.

--- index.js
+++ index.js
@@ -1,38 +1,13 @@
-// Import datejs
-require('datejs');
-
-function combineUsers(...args) {
-  // Initialize return object
-  const combinedObject = {
-    users: []
-  };
-
-  // Merge all arrays into one
-  args.forEach(arr => {
-    combinedObject.users.push(...arr);
-  });
-
-  // Add today's date in M/d/yyyy format
-  combinedObject.merge_date = new Date().toString('M/d/yyyy');
-
-  // Return the result
-  return combinedObject;
-}
-
-// Export for testing
-module.exports = combineUsers;
-require('datejs');
-
 function combineUsers(...args) {
   const combinedObject = {
-    users: []
+    users: [],
   };
 
   args.forEach(arr => {
     combinedObject.users.push(...arr);
   });
 
-  combinedObject.merge_date = new Date().toString('M/d/yyyy');
+  combinedObject.merge_date = new Date().toDateString();
 
   return combinedObject;
 }
